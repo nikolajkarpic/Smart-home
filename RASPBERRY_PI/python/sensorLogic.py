435e558cb79467cb3ac3d51f7f48a6f1a91bc37e
Nikolaj <karpicnikolaj@gmail.com>
2022-04-13 19:25:05 +0200

    Tested serial communication via python

---
+++ RASPBERRY_PI/python/sensorLogic.py
@@ -0,0 +1,16 @@
+import serial
+
+serialPort = serial.Serial('COM4', baudrate=9600)
+serialString = ""  # Used to hold data coming over UART
+while 1:
+    # Wait until there is data waiting in the serial buffer
+    if serialPort.in_waiting > 0:
+
+        # Read data out of the buffer until a carraige return / new line is found
+        serialString = serialPort.readline()
+
+        # Print the contents of the serial data
+        try:
+            print(serialString.decode("Ascii"))
+        except:
+            pass
