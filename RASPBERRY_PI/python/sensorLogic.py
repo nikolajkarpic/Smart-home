93a9913641cedc6436a1fe487c3b59db46cef120
Nikolaj <karpicnikolaj@gmail.com>
2022-04-15 15:29:24 +0200

    Added git ignore file, began serial communication files

--- RASPBERRY_PI/python/sensorLogic.py
+++ RASPBERRY_PI/python/sensorLogic.py
@@ -2,33 +2,82 @@ from urllib import request
 import serial
 import requests
 
-serialPort = serial.Serial('COM4', baudrate=9600)
-serialString = ""  # Used to hold data coming over UART
-data = ""
-dataFromHttp = ""
-while 1:
-    # Wait until there is data waiting in the serial buffer
-    if serialPort.in_waiting > 0:
 
-        dataFromHttp = requests.get(
-            'https://jsonplaceholder.typicode.com/posts')
-        print(dataFromHttp.text)
-        # Read data out of the buffer until a carraige return / new line is found
-        serialString = serialPort.readline()
+def splitStringToDict(sensorData):
+    sensorData = sensorData[:-2]
+    sensorData = sensorData.split('|')
+    digitalPins = sensorData[0].split()
+    analoguePins = sensorData[1].split()
+    digitalPinsDict = {}
+    analoguePinsDict = {}
+    print(digitalPins)
+    print(analoguePins)
 
-        try:
-            data = serialString.decode("Ascii")
-        except:
-            pass
+    for data in digitalPins:
+        [key, value] = data.split(':')
+        digitalPinsDict[key] = (int(value))
 
-        sensorData = data.split()
+    for data in analoguePins:
+        [key, value] = data.split(':')
+        analoguePinsDict[key] = (float(value))
 
-        print(sensorData)
+    return [digitalPinsDict, analoguePinsDict]
 
-        # Print the contents of the serial data
-        try:
-            print(serialString.decode("Ascii"))
+# doesnt work as of now... implement class bassed structure.
 
-        except:
-            pass
-    serialPort.write('A'.encode())
+
+def readFromFIle():
+    file = open('file.txt', 'r')
+    oldData = ''
+    newData = file.read()
+    if (newData == oldData):
+        return
+    print(newData)
+    oldData = newData
+    file.close()
+
+
+def main():
+    serialPort = serial.Serial('COM4', baudrate=9600)
+    serialString = ""  # Used to hold data coming over UART
+    data = ""
+    dataFromHttp = ""
+    digitalPinsDict = {}
+    analoguePinsDict = {}
+    newData = ""
+    oldData = ""
+    while 1:
+        readFromFIle()
+        # Wait until there is data waiting in the serial buffer
+        if serialPort.in_waiting > 0:
+
+            # place holder for sending data to database / backend
+            # dataFromHttp = requests.get(
+            #     'https://jsonplaceholder.typicode.com/posts')
+            # print(dataFromHttp.text)
+            # Read data out of the buffer until a carraige return / new line is found
+            serialString = serialPort.readline()
+
+            try:
+                newData = serialString.decode("Ascii")
+            except:
+                pass
+
+            [digitalPinsDict, analoguePinsDict] = splitStringToDict(newData)
+
+            # afted you're done testing enable this, so that the server doesnt send unnesccecary data
+            # if(newData == oldData):
+            #     continue
+            print(digitalPinsDict)
+            print(analoguePinsDict)
+            try:
+                print(serialString.decode("Ascii"))
+
+            except:
+                pass
+        serialPort.write('A'.encode())
+
+        oldData = newData
+
+
+main()
