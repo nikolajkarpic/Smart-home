232bfb62bed6e8c90749650d9cc33a68a5d7a295
Nikolaj <karpicnikolaj@gmail.com>
2022-04-26 18:55:03 +0200

    Added checking for new commands every second.

--- RASPBERRY_PI/python/sensorLogic.py
+++ RASPBERRY_PI/python/sensorLogic.py
@@ -1,5 +1,7 @@
+from distutils.command.check import check
 from re import L
 from time import sleep
+from tkinter.tix import Tree
 from urllib import request
 import serial
 import requests
@@ -7,10 +9,10 @@ import json
 from datetime import datetime
 
 
-def chehkTime(timeT):
+def chehkTime(time, period):
     nowT = datetime.now().second
     # print(nowT, " ", timeT)
-    if (abs(int(nowT) - int(timeT)) >= 5):
+    if (abs(int(nowT) - int(time)) >= period):
         return True
 
 
@@ -37,68 +39,144 @@ def splitStringToDict(sensorData):
 # doesnt work as of now... implement class bassed structure.
 
 
-def doorLogic(newData, oldData):
-    if(newData == oldData):
-        return
+def calculateTempPWM(currentTemp, preferredTemp):  # returns [coolPWM, heatPWM]
+
+    if(currentTemp - preferredTemp >= 5):
+        return[1, 0]
+    if(currentTemp - preferredTemp <= -5):
+        return[0, 1]
+    value = (currentTemp - preferredTemp)/5
+    if (value >= 0):
+        return[abs(value), 0]
+    else:
+        return[0, abs(value)]
+
+
+def doorLogic(newData):
     command = ""
     value = ""
-    print(newData)
     [command, value] = newData[:-2].split(':')
     return(command, value)
 
 
-def readFromFIle():
-    file = open('file.txt', 'r')
-    oldData = ''
-    newData = file.read()
-    if (newData == oldData):
-        return
-    print(newData)
-    oldData = newData
-    file.close()
-
-
 def main():
-    nowT = datetime.now().second
-    serialPort = serial.Serial('COM5', baudrate=9600)
+    currentTime = datetime.now().second
+    lastTimeCheckCommands = datetime.now().second
+    serialPortDoor = serial.Serial('COM5', baudrate=9600)
     serialString = ""  # Used to hold data coming over UART
     data = ""
     dataFromHttp = ""
     digitalPinsDict = {}
     analoguePinsDict = {}
+    commands = ""
     newData = ""
     oldData = ""
+    jsonCommandFile = open(
+        r"C:\FTN\Diplomsi rad\misc\commandRecieved.json")
+    jsonCommandData = json.load(jsonCommandFile)
+    jsonCommandFile.close()
     com = ""
     val = ""
+    newCommandRecieved = False
+    roomCommands = []
     accesAllowed = ""
     user = ""
-    entryAtempted = False
     while 1:
-        # readFromFIle()
+        currentTime = datetime.now().second
+        if(chehkTime(lastTimeCheckCommands, 1)):
+            # instead of opening the json file, it should http request the data from database
+            jsonCommandFile = open(
+                r"C:\FTN\Diplomsi rad\misc\commandRecieved.json")
+            newjsonCommandData = json.load(jsonCommandFile)
+            if(newjsonCommandData != jsonCommandData):
+                newCommandRecieved = True  # sets it so that script knows it recieved new commands
+                # checks wethers new commnad is temp
+                if(newjsonCommandData["COMMANDS"]["PREFERRED_TEMPERATURE"] != jsonCommandData["COMMANDS"]["PREFERRED_TEMPERATURE"]):
+                    newTemp = newjsonCommandData.get(
+                        "COMMANDS").get('PREFERRED_TEMPERATURE')
+                else:
+                    newTemp = None
+
+                # Gets changes in rooms
+                for key in newjsonCommandData["COMMANDS"]["ROOMS"]:
+                    try:
+                        if(newjsonCommandData["COMMANDS"]["ROOMS"][key] != jsonCommandData["COMMANDS"]["ROOMS"][key]):
+                            print(newjsonCommandData["COMMANDS"]["ROOMS"][key])
+                            for roomStat in newjsonCommandData["COMMANDS"]["ROOMS"][key]:
+                                if (newjsonCommandData["COMMANDS"]["ROOMS"][key][roomStat] != jsonCommandData["COMMANDS"]["ROOMS"][key][roomStat]):
+                                    roomChage = [
+                                        key, {roomStat: newjsonCommandData["COMMANDS"]["ROOMS"][key][roomStat]}]
+
+                            roomCommands.append(roomChage)
+                    except:  # adds new room if it wasnt in the old data base
+                        jsonCommandData["COMMANDS"]["ROOMS"][key] = {
+                            "LIGHTS": False, "PREFERRED_TEMPERATURE": None}
+                        roomCommands.append([key, {"LIGHTS": False}])
+
+                # locks or unlocks door
+                if(newjsonCommandData["COMMANDS"]["DOOR"] != jsonCommandData["COMMANDS"]["DOOR"]):
+                    doorLocked = newjsonCommandData["COMMANDS"]["DOOR"]
+                    print(doorLocked)
+                else:
+                    doorLocked = None
+            else:
+                newCommandRecieved = False
+            jsonCommandData = newjsonCommandData
+            lastTimeCheckCommands = datetime.now().second
+
+        if(newCommandRecieved):
+            if(doorLocked != None):
+                if(doorLocked):
+                    serialPortDoor.flush()
+                    waitNumBytes = serialPortDoor.write(b'L')
+                    for x in range(0, waitNumBytes * 8):
+                        sleep(0.02)
+                    serialPortDoor.flush()
+                else:
+                    serialPortDoor.flush()
+                    waitNumBytes = serialPortDoor.write(b'U')
+                    for x in range(0, waitNumBytes * 8):
+                        sleep(0.02)
+                    serialPortDoor.flush()
+            if(newTemp != None):
+                print(calculateTempPWM(27, newTemp))
+            newCommandRecieved = False
+            # first you have to fix second arduino
+            # if(roomCommands != []):
+            #     for [room, command] in roomCommands:
+            #         if(room == "BED_ROOM_0"):
+            #             if (command.get("LIGHTS")):
+            #                 serialPortDoor.flush()
+            #                 waitNumBytes = serialPortDoor.write(b'Q1')
+            #                 for x in range(0, waitNumBytes * 8):
+            #                     sleep(0.02)
+            #                 serialPortDoor.flush()
+            #             else:
+            #                 serialPortDoor.flush()
+            #                 waitNumBytes = serialPortDoor.write(b'Q0')
+            #                 for x in range(0, waitNumBytes * 8):
+            #                     sleep(0.02)
+            #                 serialPortDoor.flush()
+
         # Wait until there is data waiting in the serial buffer
-        if serialPort.in_waiting > 0:
+        if serialPortDoor.in_waiting > 0:
 
             # place holder for sending data to database / backend
             # dataFromHttp = requests.get(
             #     'https://jsonplaceholder.typicode.com/posts')
             # print(dataFromHttp.text)
             # Read data out of the buffer until a carraige return / new line is found
-            serialString = serialPort.readline()
+            serialString = serialPortDoor.readline()
 
             try:
                 newData = serialString.decode("Ascii")
-            except:
-                pass
-            try:
-                print(serialString.decode("Ascii"))
-
             except:
                 pass
             # if(newData[0] == 'D'):
             #     [digitalPinsDict, analoguePinsDict] = splitStringToDict(
             #         newData)
             try:
-                [com, val] = doorLogic(newData, oldData)
+                [com, val] = doorLogic(newData)
             except:
                 pass
             jsonFile = open(r"C:\FTN\Diplomsi rad\misc\users.json")
@@ -114,43 +192,28 @@ def main():
             if(com == "PIN" or com == "RFID"):
                 if(accesAllowed):
                     print(user + " je usao u kucu!")
-                    serialPort.flush()
-                    waitNumBytes = serialPort.write(b'U')
+                    serialPortDoor.flush()
+                    waitNumBytes = serialPortDoor.write(b'U')
                     for x in range(0, waitNumBytes * 8):
                         sleep(0.02)
-                    serialPort.flush()
+                    serialPortDoor.flush()
 
                 else:
                     if (user == ""):
                         print("Neko je pokusao da udje u kucu!")
                     else:
                         print(user + " je pokusao da udje u kucu!")
-                    serialPort.flush()
-                    waitNumBytes = serialPort.write(b'D')
+                    serialPortDoor.flush()
+                    waitNumBytes = serialPortDoor.write(b'D')
                     for x in range(0, waitNumBytes * 8):
                         sleep(0.02)
-                    serialPort.flush()
+                    serialPortDoor.flush()
             accesAllowed = False
             com = ""
             val = ""
             user = ""
 
             jsonFile.close()
-            # afted you're done testing enable this, so that the server doesnt send unnesccecary data
-            # if(newData == oldData):
-            #     continue
-            # print(digitalPinsDict)
-            # print(analoguePinsDict)
-
-        # serialPort.write('A'.encode())
-        # if(chehkTime(nowT)):
-        #     waitNumBytes = serialPort.write(b'E')
-        #     for x in range(0, waitNumBytes * 8):
-        #         sleep(0.02)
-        #     serialPort.flush()
-        #     nowT = datetime.now().second
-        #     print("eneblovao sve ulaze")
-
         oldData = newData
 
 
